a16f5985c7ab6271df2a2c765a7e118749433eb7
compwron <wordymusic@gmail.com>
2013-03-05 21:54:30 -0600

    solution using override and addition of words to a String object

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,1 +1,15 @@
-(-16..16).each { |num| puts (num%3==0) ? ( (num%5==0) ? "FizzBuzz" : "Fizz" ) : (num%5==0) ? "Buzz" : num }
+(-16..16).each { |num| 
+	output = num
+
+	output = "Fizz" if (num%3==0)
+
+	if (num%5==0) then
+		if output == num then 
+			output = "Buzz"
+		else
+			output += "Buzz"
+		end
+	end
+
+	puts output 
+}
